ac044849b825128e5cd8eedea5dec20342a873ea
Monica Huang <monicahuang@wireless-10-146-162-85.public.utexas.edu>
2015-10-12 19:33:58 -0500

    no message

---
+++ 02 Data Wrangling/Final Grades.R
@@ -0,0 +1,8 @@
+require("jsonlite")
+require("RCurl")
+# Change the USER and PASS below to be your UTEid
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from FINAL_GRADE"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_mh42375', PASS='orcl_mh42375', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+df
+summary(df)
+head(df)
+
