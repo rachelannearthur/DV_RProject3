5c4d7cea73282750935adb7bdbf836ff3dad03c8
Rachel Arthur <rarthur@rmn.com>
2015-10-15 17:53:21 -0500

    yayyyy

--- 02 Data Wrangling/Data Joining.R
+++ 02 Data Wrangling/Data Joining.R
@@ -5,6 +5,20 @@ require(dplyr)
 names(final_grades)[names(final_grades)=="SCHOOLNUMBER"] <- "SCHOOL_CODE"
 names(final_grades)
 
-join_df <- dplyr::inner_join(final_grades, enrl_working, by="SCHOOL_CODE", copy=TRUE)
+df <- transform(join_df, SCHOOL_CODE = as.numeric(paste(SCHOOL_CODE)))
+join_df <- dplyr::inner_join(final_grades, enrl_working, k_12_frl, by="SCHOOL_CODE", copy=TRUE)
+
+# Convert Columns to correct type 
+indx <- setdiff(names(join_df),true_names)
+join_names = c("SCHOOLNAME", "DISTRICTNAME", "EMH", "EMH_COMBINED", "INITIAL_PLANTYPE", "FINAL_PLANTYPE", "NOTES", "ORGANIZATION_NAME", "SCHOOL_NAME")
+join_df[indx] <- lapply(join_df[indx], function(x) as.numeric(as.character(x)))
+join_df[join_names] <- lapply(join_df[true_names], function(x) as.character(x))
+
+k_names = c("DISTRICT_NAME", "SCHOOL_NAME")
+k_nums = c("DISTRICT_CODE","SCHOOL_CODE")
+k_12_frl[k_names] <- lapply(k_12_frl[k_names], function(x) as.character(x))
+k_12_frl[k_nums] <- lapply(k_12_frl[k_nums], function(x) as.numeric(as.character(x)))
 
 final_df <- dplyr::inner_join(join_df, k_12_frl, by="SCHOOL_CODE", copy=TRUE)
+str(final_df)
+
